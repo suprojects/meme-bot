dfb8f819a361e044d3c01d0b79c88c87c16d2e49
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-11 20:36:54 +0530

    remove userid and chatid.

--- handlers/error.py
+++ handlers/error.py
@@ -4,27 +4,24 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 def error(update, context):
 
-    context.bot.send_message(chat_id = LOG_CHAT, text = ("""
+    context.bot.send_message(chat_id = LOG_CHAT, text = (f"""
 
-#{username}
-
-User ID: {userid}
-Chat ID: {chatid}
+#{context.bot.username}
 
 Error:
-{error}
+{context.error}
 
 Update:
 {update}
 
-""").format(username=context.bot.username, userid = update.message.from_user.id if update.message.from_user else '', chatid = update.message.chat.id if update.message.chat else '', error=context.error, update = update))
+""")
 
     try:
         if context.error.message == "Have no rights to send a message":
             update.message.chat.leave()
 
         else:
-            update.message.reply_text("⚠ An unexpected error occured, the error report was forwarded to the developers.", reply_markup = ([[delete_button]]))  
+            update.message.reply_text("⚠ An unexpected error occured, the error report was forwarded to the developers.")  
     except:
         pass
 
