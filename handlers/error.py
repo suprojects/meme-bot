91d4454c8c357cfb41fb48bff46e706bec053f5b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-12 14:25:09 +0530

    add reply text if can't send pics. remove logging of such errors

--- handlers/error.py
+++ handlers/error.py
@@ -8,6 +8,15 @@ def error(update, context):
         if context.error.message == "Conflict: terminated by other getUpdates request; make sure that only one bot instance is running":
             return
 
+        elif context.error.message == "Have no rights to send a message":
+            update.message.chat.leave()
+            return
+
+        elif context.error.message == "Not enough rights to send photos to the chat":
+            update.message.reply_text("I cannot send images here 🖼. How can you expect me to send a meme (which is obviously a pic)!\nI am leaving tis dum group.\n\nAnd here is a offer you can't (and should never) refuse:\n\n1. Enable permissions to send photos.\n2. Make me an admin\n3. Don't make memes in your life!")
+            update.message.chat.leave()
+            return
+
         else:
             pass
     except:
@@ -24,15 +33,6 @@ Update:
 {update}
 
 """))
-
-    try:
-        if context.error.message == "Have no rights to send a message":
-            update.message.chat.leave()
-
-        else:
-            update.message.reply_text("⚠ An unexpected error occured, the error report was forwarded to the developers.")  
-    except:
-        pass
 
 
 __handlers__ = [
