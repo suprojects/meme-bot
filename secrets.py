ab31520e16307892b9c1ee71e26efd5ba4dea9a1
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-14 14:09:04 +0530

    made secrets for sudo users

--- secrets.py
+++ secrets.py
@@ -5,7 +5,6 @@ if os.path.exists('tokens.py'):
     from tokens import *
 
     os.environ['BOT_TOKEN'] = BOT_TOKEN
-    os.environ['SUDO_USERS'] = SUDO_USERS
     os.environ['LOG_ID'] = LOG_ID
     os.environ['IMGFLIP_URL'] = IMGFLIP_URL
     os.environ['IMGFLIP_USERNAME'] = IMGFLIP_USERNAME
@@ -13,7 +12,7 @@ if os.path.exists('tokens.py'):
 
 IMGFLIP_URL = os.environ.get('IMGFLIP_URL')
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-SUDO_USERS = int(os.environ.get("SUDO_USERS"))
+SUDO_USERS = 1178472788,
 LOG_CHAT = os.environ.get("LOG_ID")
 IMGFLIP_USERNAME = os.environ.get("IMGFLIP_USERNAME")
 IMGFLIP_PASSWORD = os.environ.get("IMGFLIP_PASSWORD")
