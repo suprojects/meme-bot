14e71906aa73db23c6a73861429ebe29bf7e194b
Alpha <43486986+sudoAlphaX@users.noreply.github.com>
2021-01-22 12:13:53 +0530

    add unknown cmd handler and use cache time

--- handlers/z_othercmds.py
+++ handlers/z_othercmds.py
@@ -6,22 +6,40 @@ from secrets import THUMB_PIC, HOMEPIC as homepic
 
 from uuid import uuid4
 
+def errorinline(update, context):
+
+    result = [
+        InlineQueryResultArticle(
+            id = uuid4(),
+            title = 'Unknown Command',
+            description = "(╯°□°）╯︵ ┻━┻)",
+            input_message_content = InputTextMessageContent("❌ Unrecognized Command"),
+            thumb_url = 'https://telegra.ph/file/f13d453c19bf9beb3325c.png',
+        ),
+    ]
+
+    update.inline_query.answer(result)
+
+
+
 def blackinline(update, context):
 
     result = [
         InlineQueryResultArticle(
             id = uuid4(),
-            title = f"Meme Generator",
+            title = f"Generate memes",
             description = f"Type 'meme' to generate a meme",
-            input_message_content = InputTextMessageContent(f"💬 Inline Meme Generation help"),
+            input_message_content = InputTextMessageContent("💬 Inline Meme Generation help"),
             thumb_url = THUMB_PIC,
             reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text = 'Click here', url = create_deep_linked_url(context.bot.username, 'help_inlinememe', False))]]),
         ),
     ]
 
-    update.inline_query.answer(result, cache_time = 0)
+    update.inline_query.answer(result)
+
 
 
 __handlers__ = [
+    [InlineQueryHandler(callback = errorinline, pattern = ('\w'), run_async = True)],
     [InlineQueryHandler(callback = blackinline, run_async = True)],
 ]
